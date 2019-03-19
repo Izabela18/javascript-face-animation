85fb29e92cde1d55a9fae1075c11a8566b4163dd
Izabela Walczak <iza.niznik@gmail.com>
2019-03-19 17:41:21 +0100

    update

---
+++ tracker-script.js
@@ -0,0 +1,68 @@
+const AREA = document.body;
+const CIRCLE = document.querySelector('.circle');
+const SQUARE1 = document.querySelector('#square1');
+const SQUARE2 = document.querySelector('#square2');
+const MOON = document.querySelector('#moon');
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+function mouseCoordinates(e) {
+    // Capture the event object (mouse movement).
+    // Get X coordinate (distance from left viewport edge) via clientX property.
+    // Take total window width, subtract current coordinate and width of circle.
+    // Do the same for Y coordinate (distance from top viewport edge).
+    var horizontalPosition = windowWidth - e.clientX - 330;
+    var verticalPosition= windowHeight - e.clientY - 5;
+
+    var horizontalPositions = windowWidth - e.clientX - 200;
+    var verticalPositions= windowHeight - e.clientY - 200;
+
+    var horizontalPositions2 = windowWidth - e.clientX - 500;
+    var verticalPositions2= windowHeight - e.clientY - 200;
+
+    var horizontalPositionm = windowWidth - e.clientX - 400;
+    var verticalPositionm= windowHeight - e.clientY - 50;
+
+    // Set horizontal and vertical position.
+    CIRCLE.style.left = horizontalPosition + 'px';
+    CIRCLE.style.top = verticalPosition + 'px';
+
+    SQUARE1.style.left = horizontalPositions + 'px';
+    SQUARE1.style.top = verticalPositions + 'px';
+
+    SQUARE2.style.left = horizontalPositions2 + 'px';
+    SQUARE2.style.top = verticalPositions2 + 'px';
+
+    MOON.style.left = horizontalPositionm + 'px';
+    MOON.style.top = verticalPositionm + 'px';
+}
+
+function changeColorOnTouch() {
+    CIRCLE.style.backgroundColor = "green";
+    CIRCLE.style.borderColor = "green";
+
+    SQUARE1.style.backgroundColor = "blue";
+    SQUARE1.style.borderColor = "blue";
+
+    SQUARE2.style.backgroundColor = "blue";
+    SQUARE2.style.borderColor = "blue";
+
+    MOON.style.boxShadow  = "15px 60px 0px 0px red";
+
+}
+
+// When the mouse moves, run the mouseCoordinates function.
+AREA.addEventListener('mousemove', mouseCoordinates, false);
+
+// When the mouse touches the circle, run the changeColorOnTouch function.
+CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+SQUARE1.addEventListener('mouseenter', changeColorOnTouch, false);
+SQUARE2.addEventListener('mouseenter', changeColorOnTouch, false);
+MOON.addEventListener('mouseenter', changeColorOnTouch, false);
+
+// When the mouse leaves the circle, remove inline styles with an anonymous function.
+CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+SQUARE1.addEventListener('mouseleave', function(){SQUARE1.removeAttribute("style");}, false);
+SQUARE2.addEventListener('mouseleave', function(){SQUARE2.removeAttribute("style");}, false);
+MOON.addEventListener('mouseleave', function(){MOON.removeAttribute("style");}, false);
